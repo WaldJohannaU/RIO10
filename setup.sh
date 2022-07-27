4e3a8e476ff86b82236b5e5bc0c79514d842f25f
PilJoong, in CLO Office <43630935+piljoong-jeong@users.noreply.github.com>
2022-07-27 20:57:45 +0900

    tar -> unzip to uncompressing .zip files
    note: `tar` does not support uncompressing `.zip` files, see https://unix.stackexchange.com/questions/146206/why-cant-tar-extract-zip-files

--- setup.sh
+++ setup.sh
@@ -13,13 +13,15 @@ if [[ ! -d "data/predictions" ]]; then
 	if [[ ! -f "data/predictions/predictions.zip" ]]; then
 		wget "http://campar.in.tum.de/public_datasets/RIO10/predictions.zip" -P data/predictions
 	fi
-	tar -xvf "data/predictions/predictions.zip" -C data/predictions
+	
+	unzip "data/predictions/predictions.zip" -d data/predictions
 	rm data/predictions/predictions.zip
 fi
 
 if [[ ! -f "data/seq10" ]]; then
 	wget "http://campar.in.tum.de/public_datasets/RIO10/rio10_val.zip" -P "data"
-	tar -xvf "data/rio10_val.zip" -C "data"
+	
+	unzip "data/rio10_val.zip" -d data
 	rm data/rio10_val.zip
 fi
 
