cbab7e23b292ad09baec629fac6b94da3f289587
PilJoong, in CLO Office <43630935+piljoong-jeong@users.noreply.github.com>
2022-08-12 19:59:45 +0900

    shebang

---
+++ setup_original.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+# Install python dependencies
+pip install -r requirements.txt
+
+# download data
+if [[ ! -d "data/errors" ]]; then
+	mkdir data/errors
+fi
+
+if [[ ! -d "data/predictions" ]]; then
+	mkdir data/predictions
+	if [[ ! -f "data/predictions/predictions.zip" ]]; then
+		wget "http://campar.in.tum.de/public_datasets/RIO10/predictions.zip" -P data/predictions
+	fi
+	tar -xvf "data/predictions/predictions.zip" -C data/predictions
+	rm data/predictions/predictions.zip
+fi
+
+if [[ ! -f "data/seq10" ]]; then
+	wget "http://campar.in.tum.de/public_datasets/RIO10/rio10_val.zip" -P "data"
+	tar -xvf "data/rio10_val.zip" -C "data"
+	rm data/rio10_val.zip
+fi
+
+echo "Sucessfully setup."
